799a4b4e0ef73bfb3e0f662dcc358833f80e75fc
sxyoo <109959956+RamaBotz19@users.noreply.github.com>
2022-08-23 18:48:36 +0800

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,16 +3,16 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285850539404:79609055704@s.whatsapp.net
-item1.X-ABLabel:owner selalu ada 
-item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+FN: Mass Aji>3
+item.ORG: Mass Aji>3
+item1.TEL;waid=6287793236775:79609055704@s.whatsapp.net
+item1.X-ABLabel:NEED DOI CEWE
+item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCJNi-zXIsky4nheONnxoY1w
 item2.X-ABLabel:YOUTUBE
 item3.ADR:;;🎍AMERIKA SERIKAT;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
-item4.URL:https//github.com/Alpiii22 
+item4.URL:https//github.com/RamaBotz19 
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
@@ -24,8 +24,8 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-let jarot = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg'
-await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jngn di spam kack', 'Store', '.store', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
+let jarot = 'https://telegra.ph/file/0321666c0d11259297013.jpg'
+await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor ayng gw, mksudnya owner gw*`, '📮: Lagi need Ayng cewe ka', 'Store', '.store', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
 
