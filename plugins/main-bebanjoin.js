57239e23e77651dd877c2cde92f3ae941607aec6
sxyoo <109959956+RamaBotz19@users.noreply.github.com>
2022-08-23 18:52:24 +0800

    Update main-bebanjoin.js

--- plugins/main-bebanjoin.js
+++ plugins/main-bebanjoin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/2f08d4a54fa18d67d04d6.png', m, { packname: "Oscarbotz", author: "@jarot" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/2f08d4a54fa18d67d04d6.png', m, { packname: "𝘚𝘟𝘠𝘖𝘉𝘖𝘛𝘡", author: "@AJI" })
 }
 
 handler.customPrefix = /^(bebanjoin)$/i
